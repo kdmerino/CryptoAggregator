3291d5be57421e8ba93a1ec54da85176bbfc3c74
Kevin <kmerino@berkeley.edu>
2021-06-19 11:19:13 -0700

    added exchange

---
+++ src/main/java/com/AssetArrange/CryptoAggregator/Constants/Constants.java
@@ -0,0 +1,5 @@
+package com.AssetArrange.CryptoAggregator.Constants;
+
+public class Constants {
+    public static final String HMAC_SHA256 = "HmacSHA256";
+}
---
+++ src/test/java/com/AssetArrange/CryptoAggregator/CryptoAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AssetArrange.CryptoAggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
